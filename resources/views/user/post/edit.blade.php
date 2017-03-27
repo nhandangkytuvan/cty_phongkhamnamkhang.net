9653856b09c832378590fdeecaf4503b84c0692a
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-03-27 11:10:21 +0700

    ok

--- resources/views/user/post/edit.blade.php
+++ resources/views/user/post/edit.blade.php
@@ -109,7 +109,7 @@
                             content_css: [
                                 '{{ asset("public/font/codepen.min.css") }}'
                             ],
-                            imagetools_cors_hosts: ['//www.phongkhamnamkhang.net'],
+                            imagetools_cors_hosts: ['//http://localhost/cty_phongkhamnamkhang.net'],
                         });
                     });
                 </script>
