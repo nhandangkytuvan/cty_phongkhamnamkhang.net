34e74ee59d5197d30825188cb0724c08c54856da
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-04-08 08:53:38 +0700

    ok

--- resources/views/web/mobile/search.blade.php
+++ resources/views/web/mobile/search.blade.php
@@ -3,9 +3,9 @@
     <title>Tìm kiếm - {{ $setting->web_name }}</title>
 @endsection('title')
 @section('keyword')
-<meta property="og:image" content="{{ $setting->web_logo }}">
-<meta id="metaDescription" name="description" content="">
-<meta id="metaKeywords" name="keywords" content="">
+<meta property="og:image" content="{{ asset('public/img/'.$setting->web_logo) }}">
+<meta id="metaDescription" name="description" content="{{ $setting->web_description }}">
+<meta id="metaKeywords" name="keywords" content="{{ $setting->web_keyword }}">
 @endsection('keyword')
 @section('css')
     <link rel="stylesheet" href="{{ asset('public/css/mobile/mobile-pagination.css') }}">
