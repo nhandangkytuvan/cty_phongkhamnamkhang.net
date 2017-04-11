c439ed8a2a6035771430ec870b8a89323c3933e5
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-04-11 10:22:28 +0700

    ok

--- app/Listeners/ViewSumListener.php
+++ app/Listeners/ViewSumListener.php
@@ -6,6 +6,7 @@ use App\Events\ViewSumEvent;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Setting;
+use Session;
 class ViewSumListener
 {
     /**
@@ -26,8 +27,36 @@ class ViewSumListener
      */
     public function handle(ViewSumEvent $event)
     {
-        $setting = $event->setting;
-        $setting->web_visitday = $setting->web_visitday + 1;
-        $setting->save();
+        if(!Session::has('use')){
+            if(rand(0,9)>=3){
+                $setting = $event->setting;
+                $setting->web_visitday = $setting->web_visitday + 1;
+                $setting->save();
+                //visit today
+                $web_visitday = Session::get('web_visitday');
+                $web_visitday = implode('',$web_visitday)+1;
+                $web_visitday = str_split($web_visitday);
+                if(count($web_visitday)==1){
+                    array_unshift($web_visitday,'0','0');
+                }
+                if(count($web_visitday)==2){
+                    array_unshift($web_visitday,'0');
+                }
+                Session::put('web_visitday',$web_visitday);
+                //dathen today
+                if(rand(0,9)>=2){
+                    $web_dathen = Session::get('web_dathen');
+                    $web_dathen = implode('',$web_dathen)+1;
+                    if($web_dathen>99){
+                        $web_dathen = 99;
+                    }
+                    $web_dathen = str_split($web_dathen);
+                    if(count($web_dathen)==1){
+                        array_unshift($web_dathen,'0');
+                    }
+                    Session::put('web_dathen',$web_dathen);
+                }
+            }
+        }
     }
 }
