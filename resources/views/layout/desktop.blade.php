dc4369b625ce24d6c9b443c965ca47f3c0a18da4
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-05-22 16:27:56 +0700

    ok

--- resources/views/layout/desktop.blade.php
+++ resources/views/layout/desktop.blade.php
@@ -42,6 +42,7 @@
 	<script type="text/javascript" src="{{ asset('public/js/global/toastr8/dist/js/toastr8.min.js') }}"></script>
 	@yield('js')
 	<script type="text/javascript" src="{{ asset('public/js/desktop/desktop.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('public/js/desktop/dkdt.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('public/js/desktop/right.js') }}"></script>
 	<!-- <script type="text/javascript" src="{{ asset('public/js/desktop/swt.js') }}"></script> -->
 </head>
