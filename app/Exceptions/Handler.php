a56fed4b6dac177861dc02c00472825f262c4697
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-05-27 10:03:47 +0700

    okkk

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,7 +45,25 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         //return redirect('/');
-        return parent::render($request, $exception);
+        //return parent::render($request, $exception);
+        if($this->isHttpException($exception)){
+            switch ($exception->getStatusCode()) 
+                {
+                // not found
+                case 404:
+                    return redirect('/');
+                    break;
+                // internal error
+                case 500:
+                    return redirect('/');
+                    break;
+                default:
+                    return $this->renderHttpException($exception);
+                break;
+            }
+        }else{
+            return parent::render($request, $exception);
+        }
     }
 
     /**
