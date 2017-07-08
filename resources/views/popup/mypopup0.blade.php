f9ac1a556ec5043fb051ae03866565bb19998893
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-07-08 16:44:21 +0700

    oke

--- resources/views/popup/mypopup0.blade.php
+++ resources/views/popup/mypopup0.blade.php
@@ -1,5 +1,6 @@
-<div id="my_popup">
-    <p>Bác sỹ đang yêu cầu được chát với bạn</p>
-    <a class="my_popup_close btn-cancel" href="javascript:;">Từ chối</a>
-    <a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="btn-agree">Đồng ý</a>
+<div id="my_popup" class="pos-relative">
+    <p>Dịch vụ tư vấn đặt hẹn </p>
+    <a class="btn-cancel" href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en">Tư vấn</a>
+    <a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="btn-agree">Đặt hẹn</a>
+    <span class="my_popup_close pos-absolute fa fa-window-close"></span>
 </div>
